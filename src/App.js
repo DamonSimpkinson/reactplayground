dba96b23662fc20e5900cfdcb9fa936f95cc1add
Damon Simpkinson <damonsimpkinson@gmail.com>
2019-09-20 17:54:50 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Reduxion',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
+class App extends Component {
+  render() {
+    const helloWorld = 'Welcome to the Road to Learn React';
+    const notAgain = 'Oh No!!! Not Again!!!';
+    const array = [1, 4, 9, 16];
+    const newArray = array.map(function (x) { return x * 2});
+    console.log(newArray);
+    return (
+        <div className="App">
+          {list.map(function(item) {
+            return <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span> {item.author}</span>
+              <span> {item.num_comments}</span>
+              <span> {item.points}</span>
+            </div>;
+          })}
+        </div>
+    );
+  }
 }
 
 export default App;
